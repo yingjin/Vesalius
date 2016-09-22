dfed21ee261f7372714ffc69093a1d6ecc6a0571
Ying Jin <ying.jin@rice.edu>
2016-09-21 20:19:37 -0500

    add code to detect smbus failure and re-initiate

--- vesalius.py
+++ vesalius.py
@@ -15,6 +15,9 @@ if async_mode is None:
     except ImportError:
         pass
 
+import smbus
+import sys
+import subprocess
 import time
 from threading import Thread
 from flask import Flask, render_template, session, request
@@ -27,14 +30,127 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, async_mode='eventlet')
 
+bus = smbus.SMBus(1)
+address1 = 0x08
+flag = 0
+
+'''def readNumber2():
+    number = bus.read_byte(address1)
+    return number
+'''
+
 thread = None
 
+zonegroups = {
+    0: '0',
+    1:'66',
+    2:'65',
+    3:'69',
+    4:'70',
+    5:'71',
+    6:'77',
+    7:'67,68',
+    8:'72,73',
+    9:'75',
+    10:'84',
+    11:'86',
+    12:'82,83',
+    13:'80',
+    14:'79',
+    15:'78',
+    16:'76',
+    17: '948',
+    18: '915,105,107,108',
+    19: '82,83',
+    20: '88',
+    21: '104',
+    22: '114,115,116',
+    23: '81',
+    24: '103',
+    25: '89,90',
+    26: '947',
+    27: '949',
+    28: '100',
+    29: '101',
+    30: '97',
+    31: '99',
+    32: '98',
+    33: '954',
+    34: '920,955,956,959',
+    35: '953',
+    36: '952',
+    37: '952',
+    38: '950',
+    39: '919',
+    40: '109',
+    41: '920,955,956,959',
+    42: '957,958',
+    43: '957,958',
+    44: '42',
+    45: '961',
+    46: '920,955,956,959',
+    47: '964,965',
+    48: '923,50,51,52',
+    49: '121',
+    50: '120',
+    51: '117',
+    52: '945',
+    53: '962,966,967,968,969',
+    54: '964,965',
+    55: '946',
+    56: '916,110,111,112,113',
+    57: '923,50,51,52',
+    58: '928,52',
+    59: '928,52',
+    60: '926',
+    61: '962,966,967,968,969',
+    62: '962,966,967,968,969',
+    63: '962,966,967,968,969',
+    64: '962,966,967,968,969',
+    65: '38,937',
+    66: '936',
+    67: '57',
+    68: '934,55',
+    69: '931',
+    70: '56',
+    71: '53',
+    72: '54'
+    }
+
+def readNumber1():
+    try:
+        number = bus.read_byte(address1)
+        flag = 0
+        return number
+    except IOError:
+        print("IOError in read Data")
+        subprocess.call(['i2cdetect', '-y', '1'])
+        flag = 1
+        return 0
+
+def background_thread1():
+    count = 0
+    time.sleep(0.05)
+    while True:
+        if flag == 0:
+            sig = readNumber1()
+            
+            if sig > 0 and sig < 73:
+                print sig
+                zonegroup = zonegroups[sig]
+                socketio.emit('my response',
+                         {'data': 'Server generated event', 'zonegroup': zonegroup},
+                          namespace='/vesalius5')
+            time.sleep(0.005)
+        else:
+            flag = 0
+        
 def background_thread2():
-    """Example of how to send server generated events to clients."""
+    '''"""Example of how to send server generated events to clients."""
     count = 0
     zonegroups = ('76', '76', '76', '76', '76', '76')
     time.sleep(1)
-    '''while True:
+    while True:
         count += 1
         zonegroup = zonegroups[count%6]
         socketio.emit('my response',
@@ -46,7 +162,7 @@ def background_thread2():
 def index():
     global thread
     if thread is None:
-        thread = Thread(target=background_thread2)
+        thread = Thread(target=background_thread1)
         thread.daemon = True
         thread.start()
     return render_template('main.html')
@@ -55,8 +171,6 @@ def index():
 app.add_url_rule('/metadataByZonegroup/<string:zone_group>',
     view_func=MetadataByZonegroup.as_view('metadata_by_zonegroup'), methods=['GET'])
 
-#app.add_url_rule('/metadataByZoneid/<string:zone_id>',
-#    view_func=MetadataByZoneid.as_view('metadata_by_zoneid'), methods=['GET'])
 
 
 @socketio.on('connect', namespace='/vesalius5')
